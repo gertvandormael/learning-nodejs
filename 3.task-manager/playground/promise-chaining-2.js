ef87dbdf1a26c979f724c356a7cdf0992673c5fd
gertvandormael <vandormael.gert@gmail.com>
2019-12-05 15:30:01 +0100

    Explore async and await

--- 3.task-manager/playground/promise-chaining-2.js
+++ 3.task-manager/playground/promise-chaining-2.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose")
 const Task = require("../src/models/task")
 
-Task.findByIdAndDelete("5de8ed2fb3de3423142ea4fb")
-	.then(() => {
-		console.log("User deleted")
-		return Task.countDocuments({ completed: false })
-	})
-	.then(result => {
-		console.log(result)
+// Task.findByIdAndDelete("5de8ed2fb3de3423142ea4fb")
+// 	.then(() => {
+// 		console.log("User deleted")
+// 		return Task.countDocuments({ completed: false })
+// 	})
+// 	.then(result => {
+// 		console.log(result)
+// 	})
+// 	.catch(error => {
+// 		console.log(error)
+// 	})
+
+const deleteTaskAndCount = async id => {
+	const task = await Task.findByIdAndDelete(id)
+	const count = await Task.countDocuments({ completed: false })
+	return count
+}
+
+deleteTaskAndCount("5de7d509fc0f26108c843ad8")
+	.then(count => {
+		console.log(count)
 	})
 	.catch(error => {
 		console.log(error)
--- 3.task-manager/playground/promise-chaining.js
+++ 3.task-manager/playground/promise-chaining.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose")
 const User = require("../src/models/user")
 
-User.findByIdAndUpdate("5de8e81d87b2f22fa0ab697b", { age: 1 })
-	.then(user => {
-		console.log(user)
-		return User.countDocuments({ age: 1 })
-	})
-	.then(result => {
-		console.log(result)
+// User.findByIdAndUpdate("5de8e81d87b2f22fa0ab697b", { age: 1 })
+// 	.then(user => {
+// 		console.log(user)
+// 		return User.countDocuments({ age: 1 })
+// 	})
+// 	.then(result => {
+// 		console.log(result)
+// 	})
+// 	.catch(error => {
+// 		console.log(error)
+// 	})
+
+const updateAgeAndCount = async (id, age) => {
+	const user = await User.findByIdAndUpdate(id, { age: age })
+	const count = await User.countDocuments({ age: age })
+	return count
+}
+
+updateAgeAndCount("5de8e81d87b2f22fa0ab697b", 5)
+	.then(count => {
+		console.log(count)
 	})
 	.catch(error => {
 		console.log(error)
