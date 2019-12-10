9695687dd67b8ee66c5f1e6b8b043fe6be5cfc65
gertvandormael <vandormael.gert@gmail.com>
2019-12-10 13:06:14 +0100

    Add pagination and filters to models

--- 3.task-manager/src/models/user.js
+++ 3.task-manager/src/models/user.js
@@ -4,51 +4,58 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const Task = require("./task")
 
-const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	email: {
-    type: String,
-    unique: true,
-		required: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error("Email is invalid")
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+			trim: true,
+			lowercase: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error("Email is invalid")
+				}
 			}
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 7,
-		validate(value) {
-			if (value.toLowerCase().includes("password")) {
-				throw new Error("Password can't be 'password'")
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true,
+			minlength: 7,
+			validate(value) {
+				if (value.toLowerCase().includes("password")) {
+					throw new Error("Password can't be 'password'")
+				}
 			}
-		}
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error("Age must be a postive number")
+		},
+		age: {
+			type: Number,
+			default: 0,
+			validate(value) {
+				if (value < 0) {
+					throw new Error("Age must be a postive number")
+				}
 			}
-		}
-  },
-  tokens: [{
-    token: {
-      type: String,
-      required: true
-    }
-  }]
-})
+		},
+		tokens: [
+			{
+				token: {
+					type: String,
+					required: true
+				}
+			}
+		]
+	},
+	{
+		timestamps: true
+	}
+)
 
 userSchema.virtual("tasks", {
 	ref: "Task",
@@ -56,9 +63,9 @@ userSchema.virtual("tasks", {
 	foreignField: "owner"
 })
 
-// Express uses JSON.stringify when it sends back data. With the toJSON method we can intercept this data and alter it. 
-// In this case we alter it so that the password and tokens don't get send back to the client 
-userSchema.methods.toJSON = function () {
+// Express uses JSON.stringify when it sends back data. With the toJSON method we can intercept this data and alter it.
+// In this case we alter it so that the password and tokens don't get send back to the client
+userSchema.methods.toJSON = function() {
 	const user = this
 	const userObject = user.toObject()
 
@@ -68,43 +75,43 @@ userSchema.methods.toJSON = function () {
 	return userObject
 }
 
-userSchema.methods.generateAuthToken = async function () {
-  const user = this
-  const token = jwt.sign({ _id: user._id.toString() }, "secret")
+userSchema.methods.generateAuthToken = async function() {
+	const user = this
+	const token = jwt.sign({ _id: user._id.toString() }, "secret")
 
-  user.tokens = user.tokens.concat({ token: token })
-  await user.save()
+	user.tokens = user.tokens.concat({ token: token })
+	await user.save()
 
-  return token
+	return token
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
-  const user = await User.findOne({ email: email })
+	const user = await User.findOne({ email: email })
 
-  if (!user) {
-    throw new Error("Unable to login")
-  }
+	if (!user) {
+		throw new Error("Unable to login")
+	}
 
-  const isMatch = await bcrypt.compare(password, user.password)
+	const isMatch = await bcrypt.compare(password, user.password)
 
-  if (!isMatch) {
-    throw new Error("Unable to login")
-  }
+	if (!isMatch) {
+		throw new Error("Unable to login")
+	}
 
-  return user
+	return user
 }
 
 // Hash passwords
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function(next) {
 	const user = this
-  if (user.isModified("password")) {
-    user.password = await bcrypt.hash(user.password, 8)
-  }
-  
-  next()
+	if (user.isModified("password")) {
+		user.password = await bcrypt.hash(user.password, 8)
+	}
+
+	next()
 })
 
-userSchema.pre("remove", async function (next) {
+userSchema.pre("remove", async function(next) {
 	const user = this
 	await Task.deleteMany({ owner: user._id })
 	next()
